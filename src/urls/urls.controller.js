da0b7af37cc55a4a8b9e43e14ad3418651ef55b9
PhamJimmy <JimmyPhamRes@gmail.com>
2022-04-05 17:25:12 -0700

    added urls create method and property validation

--- src/urls/urls.controller.js
+++ src/urls/urls.controller.js
@@ -1,9 +1,26 @@
 const urls = require("../data/urls-data");
 
+const hasBodyProperty = (propertyName) => {
+  const { data: { property } = {} } = req.body;
+  property ? next() : next({ status: 400, message: `Need valid property: ${property}` })
+}
+
+let highestId = urls.reduce((currMaxId, url) => Math.max(currMaxId, url.id), 0)
+
+const create = (req, res) => {
+  const { data: { href } = {} } = req.body;
+  const newUrl = {
+    id: ++highestId,
+    href,
+  }
+  urls.push()
+}
+
 const list = (req, res) => {
   res.json({ data: urls });
 }
 
 module.exports = {
-  list
+  create: [hasBodyProperty("href"), create],
+  list,
 }
